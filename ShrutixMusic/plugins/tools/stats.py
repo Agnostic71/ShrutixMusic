4a93a292281bc40b56d31be53ff9d8f5722ed332
surajitaiml711-cloud <surajitaiml711@gmail.com>
2026-06-02 08:02:04 -0700

    Update stats.py

--- ShrutixMusic/plugins/tools/stats.py
+++ ShrutixMusic/plugins/tools/stats.py
@@ -19,7 +19,7 @@ from ShrutixMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
-@nand.on_message(filters.command(["stats", "gstats"]) & filters.group & ~BANNED_USERS)
+@nand.on_message(filters.command(["stats", "gstats"]) & SUDOERS)
 @language
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
